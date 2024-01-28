e8f817d5782f5daf4c31d57ff65279cb3193d598
Qasim Khan <56044350+Qasimkhan786@users.noreply.github.com>
2024-01-29 02:43:09 +0500

    nodejs test

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = express.Router();
 
+const messagesRoutes = require('./messages.routes')
 const userRoutes = require('./user.routes');
 
-
+routes.use('/chat', messagesRoutes)
 routes.use('/user', userRoutes);
 
 
